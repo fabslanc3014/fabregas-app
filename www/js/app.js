5b728cd827a5378bf227dc18723f554dc3031bab
fabregaslanc3014 <fabregaslance3144@gmail.com>
2026-04-27 17:25:22 +0800

    Updated

--- www/js/app.js
+++ www/js/app.js
@@ -4,8 +4,7 @@ $(document).ready(function () {
 
     const App = {
         canvas: $("#app"),
-        api: "api/",
-        //api: "https://m.gohumano.com/apislim4lance/",
+        api: "https://m.gohumano.com/apislim4lance/",
         usertype: localStorage.getItem("usertype"),
         token: localStorage.getItem("token"),
 
